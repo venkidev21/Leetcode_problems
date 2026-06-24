71eb503d2941d2b82056b3f8eb2fda5951812beb
Venkatesan.E <venkatesaneswaran06@gmail.com>
2026-06-24 10:54:43 +0530

    Time: 179 ms (5.06%) | Memory: 111.5 MB (19.76%) - LeetSync

--- 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.java
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.java
@@ -5,23 +5,16 @@ class Solution {
             mp.put(i,mp.getOrDefault(i,0)+1);
 
         }
-        List<Integer> ls=new ArrayList<>(mp.keySet());
-        int r=0;
+        // List<Integer> ls=new ArrayList<>(mp.keySet());
+        int round=0;
         // System.out.println(mp);
-        for(int i:ls){
-            int k=mp.get(i);
-            if(k==1) return -1;
-            if(k==2) r++;   
-           else if(k%3==0) r+=k/3;
-           else if(k%3==1){
-            r+=((k-1)/3);
-            r++;
-           }
-           else {
-                r+=k/3;
-                r++;
-           }
+        for(int i:mp.values()){
+            System.out.print(i);
+           if(i==1) return -1;
+    
+            // if(i%3==0) round+=i/3;
+             round+=(i+2)/3;
         }
-        return r;
+        return round;
     }
 }
