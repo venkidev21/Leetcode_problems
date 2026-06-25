f7aba4bf9f4617b2e5f48c00e9fcc9d0f88f46b5
Venkatesan.E <venkatesaneswaran06@gmail.com>
2026-06-25 14:48:15 +0530

    Time: 76 ms (5.52%) | Memory: 129.4 MB (10.76%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -3,8 +3,8 @@ class Solution {
         // Arrays.sort(roads,(a,b)->b[1]-a[1]);
         Map<Integer,Integer> mp=new HashMap<>();
         for(int i[]:roads){
-            mp.put(i[1]+1,mp.getOrDefault(i[1]+1,0)+1);
-             mp.put(i[0]+1,mp.getOrDefault(i[0]+1,0)+1);
+            mp.put(i[1],mp.getOrDefault(i[1],0)+1);
+             mp.put(i[0],mp.getOrDefault(i[0],0)+1);
         }
         int arr[]=new int[n];
         int num=n;
@@ -13,10 +13,10 @@ class Solution {
         pq.addAll(mp.entrySet());
         while(!pq.isEmpty()){
             Map.Entry<Integer,Integer> e = pq.poll();
-            arr[e.getKey()-1]=num;
+            arr[e.getKey()]=num;
             num--;
         }
-        // System.out.println(Arrays.toString(arr));
+        // System.out.println(pq);
         long rs=0;
         for(int i[]:roads){
             rs+=arr[i[0]]+arr[i[1]];
