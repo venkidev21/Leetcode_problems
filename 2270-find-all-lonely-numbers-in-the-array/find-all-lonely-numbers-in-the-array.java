57b2fdd26e6ebe7df0c6e4e9331737354b32f011
Venkatesan.E <venkatesaneswaran06@gmail.com>
2026-06-30 22:00:57 +0530

    Time: 79 ms (33.28%) | Memory: 172.9 MB (5.03%) - LeetSync

--- 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.java
+++ 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.java
@@ -1,17 +1,15 @@
 class Solution {
     public List<Integer> findLonely(int[] nums) {
         Map<Integer,Integer> mp=new HashMap<>();
-        List<Integer> ls=new LinkedList<>();
+        // Arrays.sort(nums);
         for(int i:nums){
-               mp.put(i,mp.getOrDefault(i,0)+1);
-        } 
+            mp.put(i,mp.getOrDefault(i,0)+1);
+        }
+        List<Integer> ls=new LinkedList<>();
         for(int i:mp.keySet()){
-            if(mp.get(i)>1 || mp.containsKey(i-1) || mp.containsKey(i+1)) continue;
+            if(mp.containsKey(i+1) || mp.containsKey(i-1) || mp.get(i)>1) continue;
             ls.add(i);
         }
         return ls;
-
-        
-
     }
 }
