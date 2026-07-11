0ba422ff3cb76ceb2c87860a02c56da353b9a059
Venkatesan.E <venkatesaneswaran06@gmail.com>
2026-07-11 12:15:01 +0530

    Time: 55 ms (5.51%) | Memory: 89.5 MB (25.73%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -4,15 +4,23 @@ class Solution {
         for(int i:arr){
             mp.put(i,mp.getOrDefault(i,0)+1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> q=new PriorityQueue<>(
-            (a,b)->b.getValue()-a.getValue());
-        q.addAll(mp.entrySet());
-        int num=arr.length;
-        int n=arr.length;
-        int count=0;
-        while(!q.isEmpty() && num>n/2){
-            num-=q.poll().getValue();
-            count++;
+        int buket[]=new int[arr.length+1];   
+        // buket sorting
+        for(int key:mp.keySet()){
+            buket[mp.get(key)]++;
+        } 
+       int remove=0;
+       int n=arr.length;
+       int count=0;
+       System.out.println(Arrays.toString(buket));
+        for(int i=buket.length-1;i>=0;i--){
+            while(buket[i]>0){
+                remove+=i;
+                buket[i]--;
+                count++;
+                if(remove>=n/2) break;
+            }
+            if(remove>=n/2) break;
         }
         return count;
 
